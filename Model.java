e82e762142030b9a2f0495448a63feb4c5c761f6
Vinguyen-32 <65844160+Vinguyen-32@users.noreply.github.com>
2020-07-26 15:41:43 -0700

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,51 @@
+package calendar;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Model {
+	private LocalDate currentDate; // Selected date
+	private LocalDate today;
+	private LocalDate viewDate; // The date of the rendered calendar
+	private ArrayList<ChangeListener> listeners;
+	
+	public Model(LocalDate currentDate, LocalDate today) {
+		listeners = new ArrayList<ChangeListener>();
+		this.currentDate = currentDate;
+		this.today = today;
+		this.viewDate = currentDate;
+	}
+	public LocalDate getCurrentDate() {
+		return currentDate;
+	}
+	public LocalDate getToday() {
+		return today;
+	}
+	public LocalDate getViewDate() {
+		return viewDate;
+	}
+	public void setViewDate(LocalDate viewDate) {
+		this.viewDate = viewDate;
+		update();
+	}
+	public void setCurrentDate(LocalDate currentDate) {
+		this.currentDate = currentDate;
+		this.viewDate = currentDate;
+		update();
+	}
+	public void attach(ChangeListener c){
+		listeners.add(c);
+    }
+
+   /**
+    * Notify views of the changes
+   */
+	public void update(){
+		for (ChangeListener l : listeners){
+			l.stateChanged(new ChangeEvent(this));
+	    }
+	}
+}
---
+++ Tester.java
@@ -0,0 +1,28 @@
+package calendar;
+
+import java.time.LocalDate;
+
+import javax.swing.JFrame;
+
+public class Tester {
+	public static void main(String[] args) {
+//		JFrame frame = new JFrame();
+//		frame.setSize(300, 300);
+		
+		LocalDate today = LocalDate.now();
+		
+		Model model = new Model(today, today);
+		Controller controller = new Controller(model);
+//		CalendarView calendarView = new CalendarView(model, controller);
+//		model.attach(calendarView);
+//		
+//		frame.add(calendarView);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setVisible(true);
+		
+		
+		Screen screen = new Screen(model, controller);
+		
+		
+	}
+}
